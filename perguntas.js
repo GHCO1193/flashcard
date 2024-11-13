81092c46e11026594ba7323760ecefb820ba8167
GHCO1193 <00001119308008SP@al.educacao.sp.gov.br>
2024-11-13 16:15:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em extensão territorial?',
+    'A Rússia'
+)
+
+criaCartao(
+    'Charada',
+    'O que é que sempre se quebra sem ser tocado?',
+    'O silêncio'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o elemento químico com o símbolo H?',
+    'Hidrogênio'
+)
+
+criaCartao(
+    'Charada',
+    'O que é que sempre vai para frente, mas nunca anda?',
+    'O tempo'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou a Mona Lisa?',
+    'Da Vinci'
+)
